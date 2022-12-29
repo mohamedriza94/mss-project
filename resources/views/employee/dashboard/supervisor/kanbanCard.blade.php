1a93df5e47b123dc81439bc515bdd0d3abef0a87
Riza <mohamedriza94@gmail.com>
2022-12-29 09:26:23 +0530

    Kanban Card CRUD UI

--- resources/views/employee/dashboard/supervisor/kanbanCard.blade.php
+++ resources/views/employee/dashboard/supervisor/kanbanCard.blade.php
@@ -103,274 +103,139 @@
           </div>
         </header>
         <div class="kanban-cards-page-container02">
-          <button class="kanban-cards-page-button button">Create New</button>
-          <button class="kanban-cards-page-button1 button">Go To Tasks</button>
+          <a href="#formHeader" class="kanban-cards-page-button button">Create New</a>
+          <a href="#taskTableHeader" class="kanban-cards-page-button1 button">Go To Tasks</a>
         </div>
         <div id="inventorycont" class="kanban-cards-page-container03">
           <div class="kanban-cards-page-container04">
             <table width="100%">
               <thead>
                 <tr>
-                  <th>ID
-                  <th>First Name
-                  <th>Last Name
-                  <th>Goes By
-                  <th>Gender
-                  <th>Class
-                  <th>Alive
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1
-                  <td>Malcolm
-                  <td>Reynolds
-                  <td>Mal, Cap'n
-                  <td>M
-                  <td>Captain
-                  <td>Yes
-                <tr>
-                  <td>2
-                  <td>Zoe
-                  <td>Washburn
-                  <td>Zoe
-                  <td>F
-                  <td>First Mate
-                  <td>Yes
-                <tr class="disabled">
-                  <td>3
-                  <td>Hoban
-                  <td>Washburn
-                  <td>Wash
-                  <td>M
-                  <td>Pilot
-                  <td>No
-                <tr>
-                  <td>4
-                  <td>Kaylee
-                  <td>Frye
-                  <td>Kaylee
-                  <td>F
-                  <td>Mechanic
-                  <td>Yes
-                <tr>
-                  <td>5
-                  <td>Jayne
-                  <td>Cobb
-                  <td>Jayne
-                  <td>M
-                  <td>Muscle
-                  <td>Yes
-              </tbody>
-            </table>
+                  <th>No.</th>
+                  <th>Title</th>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </thead>
+                <tbody id="kanbanCardTable">
+                  
+                </tbody>
+              </table>
           </div>
           <span class="kanban-cards-page-text08">
             <span>Kanban Cards</span>
             <br />
           </span>
-          <button id="nextbtn" class="kanban-cards-page-button2 button">
+          <button id="btnNext" class="kanban-cards-page-button2 button">
             Next
           </button>
-          <button id="prevbtn" class="kanban-cards-page-button3 button">
+          <button id="btnPrev" class="kanban-cards-page-button3 button">
             Previous
           </button>
         </div>
         <div id="inventorycont" class="kanban-cards-page-container05">
-          <span class="kanban-cards-page-text11">Tasks</span>
+          <span class="kanban-cards-page-text11" id="taskTableHeader">Tasks</span>
           <div class="kanban-cards-page-container06">
             <table width="100%">
               <thead>
                 <tr>
-                  <th>ID
-                  <th>First Name
-                  <th>Last Name
-                  <th>Goes By
-                  <th>Gender
-                  <th>Class
-                  <th>Alive
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1
-                  <td>Malcolm
-                  <td>Reynolds
-                  <td>Mal, Cap'n
-                  <td>M
-                  <td>Captain
-                  <td>Yes
-                <tr>
-                  <td>2
-                  <td>Zoe
-                  <td>Washburn
-                  <td>Zoe
-                  <td>F
-                  <td>First Mate
-                  <td>Yes
-                <tr class="disabled">
-                  <td>3
-                  <td>Hoban
-                  <td>Washburn
-                  <td>Wash
-                  <td>M
-                  <td>Pilot
-                  <td>No
-                <tr>
-                  <td>4
-                  <td>Kaylee
-                  <td>Frye
-                  <td>Kaylee
-                  <td>F
-                  <td>Mechanic
-                  <td>Yes
-                <tr>
-                  <td>5
-                  <td>Jayne
-                  <td>Cobb
-                  <td>Jayne
-                  <td>M
-                  <td>Muscle
-                  <td>Yes
-              </tbody>
-            </table>
+                  <th>No.</th>
+                  <th>Name</th>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </thead>
+                <tbody id="taskTable">
+                  
+                </tbody>
+              </table>
           </div>
           <div class="kanban-cards-page-container07">
-            <button id="tasksprevbtn" class="kanban-cards-page-button4 button">
-              &lt;&lt;
+            <button id="btnPrev_Arrow" class="kanban-cards-page-button4 button">
+              <<
             </button>
-            <button id="tasksnextbtn" class="kanban-cards-page-button5 button">
-              &gt;&gt;
+            <button id="btnNext_Arrow" class="kanban-cards-page-button5 button">
+              >>
             </button>
           </div>
         </div>
         <div class="kanban-cards-page-container08">
-          <ul id="errorlist" class="kanban-cards-page-ul list">
-            <li class="kanban-cards-page-li list-item">
-              <span class="kanban-cards-page-text13">Text</span>
-            </li>
-            <li class="kanban-cards-page-li1 list-item">
-              <span class="kanban-cards-page-text14">Text</span>
-            </li>
-            <li class="kanban-cards-page-li2 list-item">
-              <span class="kanban-cards-page-text15">Text</span>
-            </li>
-          </ul>
+
+          <ul id="errorlist" class="kanban-cards-page-ul list"></ul>
+
           <div class="kanban-cards-page-container09">
-            <span class="kanban-cards-page-text16">Create Card</span>
+            <span class="kanban-cards-page-text16" id="formHeader">Create Card</span>
+
             <form class="kanban-cards-page-form">
+
+              <input type="hidden" id="id">
+
               <div class="kanban-cards-page-container10">
                 <span class="kanban-cards-page-text17">Card Number</span>
-                <input
-                  type="number"
-                  id="cardnumbertxt"
-                  name="cardnumbertxt"
-                  target="noofworkslotstxt"
-                  class="kanban-cards-page-textinput input"
-                />
+                <input type="number" id="cardnumbertxt" name="cardnumbertxt" target="noofworkslotstxt" class="kanban-cards-page-textinput input"/>
               </div>
+
               <div class="kanban-cards-page-container11">
                 <span class="kanban-cards-page-text18">Title</span>
-                <input
-                  type="text"
-                  id="titletxt"
-                  name="titletxt"
-                  class="kanban-cards-page-textinput1 input"
-                />
+                <input type="text" id="titletxt" name="titletxt" class="kanban-cards-page-textinput1 input"/>
               </div>
+
               <div class="kanban-cards-page-container12">
                 <span class="kanban-cards-page-text19">Description</span>
-                <input
-                  type="text"
-                  id="descriptiontxt"
-                  name="descriptiontxt"
-                  class="kanban-cards-page-textinput2 input"
-                />
+                <input type="text" id="descriptiontxt" name="descriptiontxt" class="kanban-cards-page-textinput2 input"/>
               </div>
-              <button
-                id="addbtn"
-                name="addbtn"
-                type="submit"
-                class="kanban-cards-page-button6 button"
-              >
-                Add
-              </button>
+
+              <div id="buttonContainer">
+                <button id="btnAdd" type="submit" class="kanban-cards-page-button6 button"> Add </button>
+                <button id="btnClear" type="submit" class="kanban-cards-page-button9 button"> All Done </button>
+              </div>
+              
             </form>
+            
           </div>
+
           <div class="kanban-cards-page-container13">
-            <span class="kanban-cards-page-text20">Add Task</span>
+            <span class="kanban-cards-page-text20" id="taskFormHeader">Add Task</span>
             <form class="kanban-cards-page-form1">
+
               <div class="kanban-cards-page-container14">
                 <span class="kanban-cards-page-text21">Card Number</span>
-                <input
-                  type="number"
-                  id="cardnumbertxt"
-                  name="cardnumbertxt"
-                  target="noofworkslotstxt"
-                  class="kanban-cards-page-textinput3 input"
-                />
+                <input type="number" id="cardnumbertxt" name="cardnumbertxt" target="noofworkslotstxt" class="kanban-cards-page-textinput3 input" />
               </div>
+
               <div class="kanban-cards-page-container15">
                 <span class="kanban-cards-page-text22">Title</span>
-                <input
-                  type="text"
-                  id="titletxt"
-                  name="titletxt"
-                  class="kanban-cards-page-textinput4 input"
-                />
+                <input type="text" id="titletxt" name="titletxt" class="kanban-cards-page-textinput4 input" />
               </div>
+
               <div class="kanban-cards-page-container16">
                 <span class="kanban-cards-page-text23">Description</span>
-                <input
-                  type="text"
-                  id="descriptiontxt"
-                  name="descriptiontxt"
-                  class="kanban-cards-page-textinput5 input"
-                />
+                <input type="text" id="descriptiontxt" name="descriptiontxt" class="kanban-cards-page-textinput5 input" />
               </div>
+
               <div class="kanban-cards-page-container17">
                 <span class="kanban-cards-page-text24">Start Date</span>
-                <input
-                  type="date"
-                  id="startdatetxt"
-                  name="startdatetxt"
-                  target="noofworkslotstxt"
-                  class="kanban-cards-page-textinput6 input"
-                />
+                <input type="date" id="startdatetxt" name="startdatetxt" target="noofworkslotstxt" class="kanban-cards-page-textinput6 input" />
               </div>
+
               <div class="kanban-cards-page-container18">
                 <span class="kanban-cards-page-text25">End Date</span>
-                <input
-                  type="date"
-                  id="enddatetxt"
-                  name="enddatetxt"
-                  target="noofworkslotstxt"
-                  class="kanban-cards-page-textinput7 input"
-                />
+                <input type="date" id="enddatetxt" name="enddatetxt" target="noofworkslotstxt" class="kanban-cards-page-textinput7 input" />
               </div>
+
               <div class="kanban-cards-page-container19">
                 <span class="kanban-cards-page-text26">Duration</span>
-                <input
-                  type="text"
-                  id="durationtxt"
-                  name="durationtxt"
-                  disabled=""
-                  class="kanban-cards-page-textinput8 input"
-                />
+                <input type="text" id="durationtxt" name="durationtxt" disabled="" class="kanban-cards-page-textinput8 input" />
               </div>
-              <button
-                id="autoschedulebtn"
-                name="autoschedulebtn"
-                type="submit"
-                class="kanban-cards-page-button7 button"
-              >
-                Auto Schedule
-              </button>
-              <button
-                id="savebtn"
-                name="savebtn"
-                type="submit"
-                class="kanban-cards-page-button8 button"
-              >
-                Save
-              </button>
+
+              <button id="autoschedulebtn" name="autoschedulebtn" type="submit" class="kanban-cards-page-button7 button" > Auto Schedule </button>
+        
+              <div id="taskButtonContainer">
+                <button id="btnAddTask" type="submit" class="kanban-cards-page-button8 button"> Save </button>
+              </div>
+
             </form>
           </div>
         </div>
